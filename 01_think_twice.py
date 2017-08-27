a68fe5d9a6c99605aac81b963b398d651a29fb07
cliffgla <cliffordgagliardo@gmail.com>
2017-08-27 14:17:02 +0200

    added alphabet

--- 01_think_twice.py
+++ 01_think_twice.py
@@ -1,10 +1,14 @@
 shift = 2
-# english_alphabet =
+english_alphabet = [
+'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+'s', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
 # german_alphabet
 #
-# alphabet = german_alphabet
+alphabet = english_alphabet
 
-list_length = 26
+list_length = len(alphabet)
 
 # take index position and printing new index position based on amount needing to be shifted
 for i in range(list_length):
