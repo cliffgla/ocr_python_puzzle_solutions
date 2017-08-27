8a59b39d38523ed2c8b111676b6d909947cc5236
cliffgla <cliffordgagliardo@gmail.com>
2017-08-27 14:49:32 +0200

    added url code

--- 01_think_twice.py
+++ 01_think_twice.py
@@ -5,8 +5,7 @@ english_alphabet = [
 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
-coded_message = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
-
+coded_message = "map"
 decoded = " "
 code = {}
 # german_alphabet
